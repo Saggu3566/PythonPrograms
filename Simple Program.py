56496dc6c4806c434c61dd5aeb417ca5d29dd4f5
JASHANPREET DHIMAN <182147548+Saggu3566@users.noreply.github.com>
2025-08-27 16:25:27 +0530

    Two Python Tasks Uploded

---
+++ Simple Program.py
@@ -0,0 +1,15 @@
+#Program of Two Numbers.
+
+a=input("Enter a number: ")
+b=input("Enter a Number: ")
+c=int(a)+int(b)
+d=int(a)-int(b)
+e=int(a)*int(b)
+f=int(a)/int(b)
+print('Addition: ',c)
+print('Subtraction: ',d)
+print('Multiplication: ',e)
+print('Division: ',f)
+
+
+
